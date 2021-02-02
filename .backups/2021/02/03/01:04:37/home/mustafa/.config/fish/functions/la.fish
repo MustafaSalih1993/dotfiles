ea7ff42df4ba9a321e8dfb960f4936d88617fe9d
mustafa salih <mustafasalih1991@gmail.com>
2021-02-03 01:10:33 +0300

    init

--- .backups/2021/02/03/01:04:37/home/mustafa/.config/fish/functions/la.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function la --wraps='ls -A --color=auto' --description 'alias la ls -A --color=auto'
-  ls -A --color=auto $argv;
-end
--- .backups/2021/02/03/01:04:37/home/mustafa/.config/fish/functions/up.fish
+++
@@ -1,6 +0,0 @@
-function up
-sudo apt update
-sudo apt list --upgradable
-sudo apt upgrade -y
-
-end
